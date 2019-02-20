137202b8613863bf068f9f9e09d29a3883bc1366
Julius Guevarra <juliusguevarra101@gmail.com>
2019-02-20 12:15:57 +0800

    Fix config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-// const workboxPlugin = require('workbox-webpack-plugin');
+const workboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = (env) => {
   const CSSExtract = new ExtractTextPlugin('css/app.css');
@@ -43,14 +43,14 @@ module.exports = (env) => {
       extensions: ['*', '.js', '.jsx']
     },
     plugins: [
-      CSSExtract
-      // new workboxPlugin.GenerateSW({
-      //   cacheId: 'crecipe',
-      //   swDest: 'sw.js',
-      //   navigateFallback: '/index.html',
-      //   clientsClaim: true,
-      //   skipWaiting: true
-      // })
+      CSSExtract,
+      new workboxPlugin.GenerateSW({
+        cacheId: 'crecipe',
+        swDest: 'sw.js',
+        navigateFallback: '/index.html',
+        clientsClaim: true,
+        skipWaiting: true
+      })
     ],
     devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
